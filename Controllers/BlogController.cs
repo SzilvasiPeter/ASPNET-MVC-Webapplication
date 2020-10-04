d2264932e86c8f294f25a872a2034ef4052dd936
SzilvasiPeter <peti.szilvasi95@gmail.com>
2020-10-04 10:59:16 +0200

    Create BlogController for CRUD operations

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+using WebApplication.Services;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BlogController : ControllerBase
+    {
+        private IBlogService myService;
+        
+        [HttpGet("/api/blogs")]
+        public ActionResult<List<Blog>> GetProducts()
+        {
+            return myService.GetAllBlogs();
+        }
+
+        [HttpPost("/api/blogs")]
+        public ActionResult<Blog> AddProduct(Blog blog)
+        {
+            myService.AddBlog(blog);
+            return blog;
+        }
+
+        [HttpPut("/api/blogs/{id}")]
+        public ActionResult<Blog> UpdateProduct(int id, Blog blog)
+        {
+            myService.UpdateBlog(id, blog);
+            return blog;
+        }
+
+        [HttpDelete("/api/blogs/{id}")]
+        public ActionResult<int> DeleteProduct(int id)
+        {
+            myService.DeleteBlog(id);
+            return id;
+        }
+    }
+}
