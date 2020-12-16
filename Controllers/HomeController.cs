2965cafa11850e73f4655da397bf378087fefde9
SzilvasiPeter <peti.szilvasi95@gmail.com>
2020-12-16 11:48:44 +0100

    Add User view and ActionResult to HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,6 +36,13 @@ namespace WebApplication.Controllers
             return View();
         }
 
+        public IActionResult Blog()
+        {
+            UserController userController = new UserController(new UserService());
+            var users = userController.GetUsers();
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
